1d304edca6910f44f0c27e018cde2d72717795de
Ivanholl <ivan.jelev@abv.bg>
2020-03-14 22:12:23 +0200

    login/signup

--- server/mongodb/userSchema.js
+++ server/mongodb/userSchema.js
@@ -1,9 +1,22 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-  name: String
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
-var User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);;
