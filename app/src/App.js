cd39c1991645a8744710095ec3b643fc43d94e13
Ivanholl <ivan.jelev@abv.bg>
2020-03-26 16:20:08 +0200

    Added sass compiling script and watcher

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,26 @@
 // import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
 
 import React, {Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-const Home = lazy(() => import('./routes/Home'));
-const About = lazy(() => import('./routes/About'));
+const Home = lazy(() => import ('./routes/Home'));
+const About = lazy(() => import ('./routes/About'));
 
 function App() {
 
-    return (<Router>
-        <Suspense fallback={<div> Loading ...</div>}>
-            <Switch>
-                <div className="container">
+    return (
+        <Router>
+
+            <Suspense fallback={<div> Loading ...</div>}>
+                <Switch>
                     <Route exact={true} path="/" component={Home}/>
                     <Route path="/about" component={About}/>
-                </div>
-            </Switch>
-        </Suspense>
-    </Router>);
+                </Switch>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
